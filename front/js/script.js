3ad197808dca3da54176c91d1fa4b9c45926a29a
JeromeV70 <villani.jerome@gmail.com>
2021-11-02 20:05:52 +0100

    affichage produits index

---
+++ front/js/script.js
@@ -0,0 +1,53 @@
+let reponse = {};
+let xmlhttp = new XMLHttpRequest();
+xmlhttp.open("GET", 'http://localhost:3000/api/products', true);
+xmlhttp.onreadystatechange = function ()
+{
+  if (xmlhttp.readyState == 4 && xmlhttp.status == 200)
+	{		
+        let reponse = xmlhttp.responseText;
+		afficheListe(reponse);
+	}
+}
+xmlhttp.send(null);
+
+function afficheListe(reponse) // afficher les produits sur la page d'accueil
+{
+	reponse = JSON.parse(reponse);
+
+	for (let i in reponse)
+	{
+		//let colors = reponse[i].colors;
+		let id = reponse[i].id;
+		let name = reponse[i].name;
+		//let price = reponse[i].price;
+		let imageUrl = reponse[i].imageUrl;
+		let description = reponse[i].description;
+		let altTxt = reponse[i].altTxt;
+
+		let a = document.createElement('a');
+		a.href = "./product.html?id="+id;
+
+		let article = document.createElement('article');
+
+		let img = document.createElement('img');
+		img.src=imageUrl;
+		img.alt=altTxt;
+
+		let h3 = document.createElement('h3');
+		h3.class="productName";
+		h3.append(name);
+
+		let p = document.createElement('p');
+		p.class = "productDescription";
+		p.append(description);
+
+		article.appendChild(img);
+		article.appendChild(h3);
+		article.appendChild(p);
+
+		a.appendChild(article);
+
+		document.getElementById('items').appendChild(a);
+	}
+}

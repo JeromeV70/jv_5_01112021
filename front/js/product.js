fad2cef200b2582be8298696f2751997c5e1a945
JeromeV70 <villani.jerome@gmail.com>
2021-11-08 21:28:27 +0100

    ajout au panier

--- front/js/product.js
+++ front/js/product.js
@@ -1,6 +1,7 @@
 let url = new URL(window.location.href);
 let _id = url.searchParams.get("id");
 let reponse = {};
+
 let xmlhttp = new XMLHttpRequest();
 xmlhttp.open("GET", 'http://localhost:3000/api/products/'+_id, true);
 xmlhttp.onreadystatechange = function ()
@@ -29,4 +30,19 @@ function afficheProduit(reponse) // afficher les caractéristiques du produit
         const option = `<option value=${couleur}>${couleur}</option>`;
         document.querySelector('#colors').insertAdjacentHTML("beforeend",option);
     }
+}
+
+document.querySelector('#addToCart').addEventListener('click',function(){ajouter(),false});
+
+function ajouter() // Ajout au panier lors du clic sur le bouton ajouter
+{
+    const couleur = document.querySelector('#colors').value;
+    const quantite = document.querySelector('#quantity').value;
+
+    let panier = new Map(JSON.parse(localStorage.getItem("panier")));
+    panier.set(_id+couleur,[_id,couleur,quantite]);
+
+    localStorage.setItem("panier",JSON.stringify(Array.from(panier)));
+
+    //console.log(panier);
 }
